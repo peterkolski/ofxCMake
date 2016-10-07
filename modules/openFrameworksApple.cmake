4ec6c6ff808114cb3efef8fedb2a96eec3dfff92
Peter A. Kolski <peter@bildpeter.de>
2016-10-07 02:49:12 +0200

    Update openFrameworksApple.cmake
    Cleanup

--- modules/openFrameworksApple.cmake
+++ modules/openFrameworksApple.cmake
@@ -114,19 +114,33 @@ set(HEADER_UTF8 ${OF_DIRECTORY}/libs/utf8cpp/include)
 set(HEADER_BOOST ${OF_DIRECTORY}/libs/boost/include)
 
 
-set(OF_CORE_HEADERS ${HEADER_OF_0} ${HEADER_OF_1} ${HEADER_OF_2} ${HEADER_OF_3} ${HEADER_OF_4} ${HEADER_OF_5} ${HEADER_OF_6} ${HEADER_OF_7} ${HEADER_OF_8} ${HEADER_OF_9} ${HEADER_OF_10} ${HEADER_OF_11} ${HEADER_POCO} ${HEADER_FREETYPE} ${HEADER_FREETYPE2} ${HEADER_FMODEX} ${HEADER_GLEW} ${HEADER_FREEIMAGE} ${HEADER_TESS2} ${HEADER_CAIRO} ${HEADER_RTAUDIO} ${HEADER_GLFW} ${HEADER_SSL} ${HEADER_UTF8} ${HEADER_BOOST})
+set(OF_CORE_HEADERS 
+        ${HEADER_OF_0} ${HEADER_OF_1} ${HEADER_OF_2} ${HEADER_OF_3} ${HEADER_OF_4} ${HEADER_OF_5} 
+        ${HEADER_OF_6} ${HEADER_OF_7} ${HEADER_OF_8} ${HEADER_OF_9} ${HEADER_OF_10} ${HEADER_OF_11} 
+        ${HEADER_POCO} 
+        ${HEADER_FREETYPE} ${HEADER_FREETYPE2} 
+        ${HEADER_FMODEX} 
+        ${HEADER_GLEW} 
+        ${HEADER_FREEIMAGE} 
+        ${HEADER_TESS2} 
+        ${HEADER_CAIRO} 
+        ${HEADER_RTAUDIO} 
+        ${HEADER_GLFW} 
+        ${HEADER_SSL} 
+        ${HEADER_UTF8} 
+        ${HEADER_BOOST})
 # ========================================================
 
 # ===================== Libs =====================
-set(glut_lib "${OF_DIRECTORY}/libs/glut/lib/osx/GLUT.framework")       # Use oF's GLUT
+set(glut_lib "${OF_DIRECTORY}/libs/glut/lib/osx/GLUT.framework")
 set(LIB_FMODEX ${OF_DIRECTORY}/libs/fmodex/lib/osx/libfmodex.dylib)
-set(LIB_GLFW ${OF_DIRECTORY}/libs/glfw/lib/osx/glfw3.a)                # Renamed in oF 0.9
+set(LIB_GLFW ${OF_DIRECTORY}/libs/glfw/lib/osx/glfw3.a)
 set(LIB_FREEIMAGE ${OF_DIRECTORY}/libs/FreeImage/lib/osx/freeimage.a)
 set(LIB_FREETYPE ${OF_DIRECTORY}/libs/freetype/lib/osx/freetype.a)
 set(LIB_GLEW ${OF_DIRECTORY}/libs/glew/lib/osx/glew.a)
 set(LIB_POCO_1 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoCrypto.a)
 set(LIB_POCO_2 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoData.a)
-set(LIB_POCO_3 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoJSON.a)           # new in oF 0.9
+set(LIB_POCO_3 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoJSON.a)
 set(LIB_POCO_4 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoDataSQLite.a)
 set(LIB_POCO_5 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoFoundation.a)
 set(LIB_POCO_6 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoNet.a)
@@ -134,7 +148,7 @@ set(LIB_POCO_7 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoNetSSL.a)
 set(LIB_POCO_8 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoUtil.a)
 set(LIB_POCO_9 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoXML.a)
 set(LIB_POCO_10 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoZip.a)
-set(LIB_POCO_11 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoMongoDB.a)       # new in oF 0.9
+set(LIB_POCO_11 ${OF_DIRECTORY}/libs/poco/lib/osx/PocoMongoDB.a)
 set(LIB_RTAUDIO ${OF_DIRECTORY}/libs/rtAudio/lib/osx/rtAudio.a)
 set(LIB_TESS ${OF_DIRECTORY}/libs/tess2/lib/osx/tess2.a)
 set(LIB_CAIRO1 ${OF_DIRECTORY}/libs/cairo/lib/osx/cairo-script-interpreter.a)
@@ -142,9 +156,9 @@ set(LIB_CAIRO2 ${OF_DIRECTORY}/libs/cairo/lib/osx/cairo.a)
 set(LIB_CAIRO3 ${OF_DIRECTORY}/libs/cairo/lib/osx/pixman-1.a)
 set(LIB_OPENSSL1 ${OF_DIRECTORY}/libs/openssl/lib/osx/crypto.a)
 set(LIB_OPENSSL2 ${OF_DIRECTORY}/libs/openssl/lib/osx/ssl.a)
-set(LIB_BOOST_1 ${OF_DIRECTORY}/libs/boost/lib/osx/boost.a)            # new in oF 0.9
-set(LIB_BOOST_2 ${OF_DIRECTORY}/libs/boost/lib/osx/boost_filesystem.a) # new in oF 0.9
-set(LIB_BOOST_3 ${OF_DIRECTORY}/libs/boost/lib/osx/boost_system.a)     # new in oF 0.9
+set(LIB_BOOST_1 ${OF_DIRECTORY}/libs/boost/lib/osx/boost.a)
+set(LIB_BOOST_2 ${OF_DIRECTORY}/libs/boost/lib/osx/boost_filesystem.a)
+set(LIB_BOOST_3 ${OF_DIRECTORY}/libs/boost/lib/osx/boost_system.a)
 
 # Updated list
 set(OF_CORE_LIBS
@@ -178,10 +192,10 @@ find_library(quicktime_lib QuickTime)
 find_library(corevideo_lib CoreVideo)
 find_library(qtkit_lib QTKit)
 find_library(cocoa_lib Cocoa)
-find_library(coremedia_lib CoreMedia)       # new in oF 0.9
-find_library(avfoundation_lib AVFoundation) # new in oF 0.9
-find_library(quartzcore_lib QuartzCore)     # new in oF 0.9
-find_library(appkit_lib AppKit)             # new in oF 0.9
+find_library(coremedia_lib CoreMedia)
+find_library(avfoundation_lib AVFoundation)
+find_library(quartzcore_lib QuartzCore)
+find_library(appkit_lib AppKit)
 
 set(OF_CORE_FRAMEWORKS
         ${accelerate_lib}
@@ -205,7 +219,7 @@ set(OF_CORE_FRAMEWORKS
 
 set(OF_SOURCE_FILES ${OF_SOURCE_FILES} ${ADDON_SOURCE_FILES})
 include_directories(${OF_CORE_HEADERS} ${OF_ADDON_HEADERS})
-set_source_files_properties(${OF_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "-x objective-c++") # Removed "-stdlib=libstdc++, added -x objective-c++
+set_source_files_properties(${OF_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "-x objective-c++")
 add_library(core OBJECT ${OF_SOURCE_FILES})
 
 add_library(static STATIC $<TARGET_OBJECTS:core>)
