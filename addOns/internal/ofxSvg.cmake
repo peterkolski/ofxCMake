35c037e2ccd07482453c9e9f6a49302236f56735
Peter Kolski <peter@bildpeter.de>
2016-10-10 20:49:54 +0200

    ofxSvg addOn

---
+++ addOns/internal/ofxSvg.cmake
@@ -0,0 +1,25 @@
+# -----------------------------------------------------------------
+# --- Script to search for all the .cpp files in the chosen folder
+# -----------------------------------------------------------------
+
+file(   GLOB_RECURSE
+        OFX_SVG_CPP
+        "${OF_DIRECTORY}/addons/ofxSvg/*.cpp"
+        )
+
+# -----------------------------------------------------------------
+# --- Set ALL directories of the addOn, which contain .h files
+# -----------------------------------------------------------------
+
+include_directories(
+        "${OF_DIRECTORY}/addons/ofxSvg/src"
+        "${OF_DIRECTORY}/addons/ofxSvg/libs/svgTiny/src"
+        )
+
+# -----------------------------------------------------------------
+# --- Set the keyword, so you can simply include the addOn
+# -----------------------------------------------------------------
+
+add_library(    ofxSvg
+        STATIC
+        ${OFX_SVG_CPP} )
--- modules/addOnsInternal.cmake
+++ modules/addOnsInternal.cmake
@@ -4,12 +4,14 @@
 #file( GLOB_RECURSE OFX_OSC_CPP  "${OF_DIRECTORY}/addons/ofxOsc/*.cpp" )
 #message( "Cpp-Tree: ${OFX_OSC_CPP}" )
 
+# TODO Comments
 include( ${OF_DIRECTORY}/CMake/addOns/internal/ofxOsc.cmake )
 include( ${OF_DIRECTORY}/CMake/addOns/internal/ofxXmlSettings.cmake )
 include( ${OF_DIRECTORY}/CMake/addOns/internal/ofxGui.cmake )
 include( ${OF_DIRECTORY}/CMake/addOns/internal/ofxKinect.cmake )
 include( ${OF_DIRECTORY}/CMake/addOns/internal/ofxNetwork.cmake )
+include( ${OF_DIRECTORY}/CMake/addOns/internal/ofxSvg.cmake )
 
-# ----- SKIZZE ---------
+# TODO automatic search for addOns .cmake files
 #file( GLOB_RECURSE ALL_ADDON_FILES  "${OF_DIRECTORY}/CMake/addOns/*.cmake" )
 #include( ALL_ADDON_FILES )
