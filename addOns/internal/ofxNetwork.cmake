bddbb175bfffd8d5bce4b71c43330328784f62b0
Peter Kolski <peter@bildpeter.de>
2016-10-10 16:52:25 +0200

    UNTESTED: ofxNetwork

---
+++ addOns/internal/ofxNetwork.cmake
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------
+# --- Script to search for all the .cpp files in the chosen folder
+# -----------------------------------------------------------------
+
+file(   GLOB_RECURSE
+        OFX_NETWORK_CPP
+        "${OF_DIRECTORY}/addons/ofxNetwork/src/*.cpp"
+        )
+
+# -----------------------------------------------------------------
+# --- Set ALL directories of the addOn, which contain .h files
+# -----------------------------------------------------------------
+
+include_directories( "${OF_DIRECTORY}/addons/ofxNetwork/src" )
+
+# -----------------------------------------------------------------
+# --- Set the keyword, so you can simply include the addOn
+# -----------------------------------------------------------------
+
+add_library(    ofxNetwork              # Set the name of the addOn
+                STATIC
+                ${OFX_NETWORK_CPP}      # Set the name
+                )
