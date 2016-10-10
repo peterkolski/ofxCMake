2fa02fee46edecdf3c9f1d646db32d718f3f3eeb
Peter Kolski <peter@bildpeter.de>
2016-10-10 21:14:29 +0200

    ofxVectorGraphics

---
+++ addOns/internal/ofxVectorGraphics.cmake
@@ -0,0 +1,27 @@
+# -----------------------------------------------------------------
+# --- Script to search for all the .cpp files in the chosen folder
+# -----------------------------------------------------------------
+
+file(   GLOB_RECURSE
+        OFX_VECTORGRAPHICS_CPP
+        "${OF_DIRECTORY}/addons/ofxVectorGraphics/*.cpp"
+        )
+
+# -----------------------------------------------------------------
+# --- Set ALL directories of the addOn, which contain .h files
+# -----------------------------------------------------------------
+
+include_directories(
+        "${OF_DIRECTORY}/addons/ofxVectorGraphics/src"
+        "${OF_DIRECTORY}/addons/ofxVectorGraphics/libs"
+)
+
+# -----------------------------------------------------------------
+# --- Set the keyword, so you can simply include the addOn
+# -----------------------------------------------------------------
+
+add_library(    ofxVectorGraphics
+        STATIC
+        ${OFX_VECTORGRAPHICS_CPP} )
+
+#TODO include Libs
