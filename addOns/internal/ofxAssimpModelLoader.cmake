518d4d088e93be9d009ebbb79262a195f92a5866
Peter Kolski <peter@bildpeter.de>
2016-10-10 21:34:04 +0200

    3dImage Loader

---
+++ addOns/internal/ofxAssimpModelLoader.cmake
@@ -0,0 +1,36 @@
+# -----------------------------------------------------------------
+# --- Script to search for all the .cpp files in the chosen folder
+# -----------------------------------------------------------------
+
+file(   GLOB_RECURSE
+        OFX_ASSIMPIMAGELOADER_CPP
+        "${OF_DIRECTORY}/addons/ofxAssimpModelLoader/*.cpp"
+        )
+
+# -----------------------------------------------------------------
+# --- Set ALL directories of the addOn, which contain .h files
+# -----------------------------------------------------------------
+
+include_directories(
+        "${OF_DIRECTORY}/addons/ofxAssimpModelLoader/src"
+        "${OF_DIRECTORY}/addons/ofxAssimpModelLoader/libs/assimp/include/assimp"
+        "${OF_DIRECTORY}/addons/ofxAssimpModelLoader/libs/assimp/include/assimp/Compiler"
+                    )
+
+# -----------------------------------------------------------------
+# --- Set the keyword, so you can simply include the addOn
+# -----------------------------------------------------------------
+
+add_library(    ofxAssimpModelLoader
+        STATIC
+        ${OFX_ASSIMPIMAGELOADER_CPP} )
+
+# -----------------------------------------------------------------
+# --- Setting a compiled lib
+# -----------------------------------------------------------------
+
+#TODO Libs for other OS (Win & Linux)
+if( APPLE )
+set(    ofxAssimpModelLoaderLib
+        "${OF_DIRECTORY}/addons/ofxAssimpModelLoader/libs/assimp/lib/assimp.a" )
+endif()
