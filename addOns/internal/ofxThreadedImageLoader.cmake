750c8cad5f0b68fafe3d81b02469f6ec43a3aca6
Peter Kolski <peter@bildpeter.de>
2016-10-10 22:05:32 +0200

    Threaded Image Loader

---
+++ addOns/internal/ofxThreadedImageLoader.cmake
@@ -0,0 +1,22 @@
+# -----------------------------------------------------------------
+# --- Script to search for all the .cpp files in the chosen folder
+# -----------------------------------------------------------------
+
+file(   GLOB_RECURSE
+        OFX_THREADEDIMAGELOADER_CPP
+        "${OF_DIRECTORY}/addons/ofxThreadedImageLoader/*.cpp"
+        )
+
+# -----------------------------------------------------------------
+# --- Set ALL directories of the addOn, which contain .h files
+# -----------------------------------------------------------------
+
+include_directories(    "${OF_DIRECTORY}/addons/ofxThreadedImageLoader/src" )
+
+# -----------------------------------------------------------------
+# --- Set the keyword, so you can simply include the addOn
+# -----------------------------------------------------------------
+
+add_library(    ofxThreadedImageLoader
+        STATIC
+        ${OFX_THREADEDIMAGELOADER_CPP} )
