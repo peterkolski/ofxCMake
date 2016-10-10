01054e9254e59477b46bb3ed7a5950846464ddb6
Peter Kolski <peter@bildpeter.de>
2016-10-10 22:50:47 +0200

    UNTESTED: Emscripten

---
+++ addOns/internal/ofxEmscripten.cmake
@@ -0,0 +1,26 @@
+# -----------------------------------------------------------------
+# --- Script to search for all the .cpp files in the chosen folder
+# -----------------------------------------------------------------
+
+file(   GLOB_RECURSE
+        OFX_EMSCRIPTEN_CPP
+        "${OF_DIRECTORY}/addons/ofxEmscripten/*.cpp"
+        )
+
+# -----------------------------------------------------------------
+# --- Set ALL directories of the addOn, which contain .h files
+# -----------------------------------------------------------------
+
+include_directories(
+        "${OF_DIRECTORY}/addons/ofxEmscripten/src"
+        "${OF_DIRECTORY}/addons/ofxEmscripten/libs/html5audio/include"
+        "${OF_DIRECTORY}/addons/ofxEmscripten/libs/html5video/include"
+        )
+
+# -----------------------------------------------------------------
+# --- Set the keyword, so you can simply include the addOn
+# -----------------------------------------------------------------
+
+add_library(    ofxEmscripten
+        STATIC
+        ${OFX_EMSCRIPTEN_CPP} )
