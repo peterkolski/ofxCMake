0ac0db1eed7c6da35942e386860f62468fdcaed5
Peter Kolski <peter@bildpeter.de>
2016-10-11 20:10:30 +0200

    Took the internal libs out - should not be used anymore in the new approach

--- modules/configApple.cmake
+++ modules/configApple.cmake
@@ -16,7 +16,7 @@ add_dependencies(${APP_NAME} of_shared)
 target_link_libraries(  ${APP_NAME}
                         $<TARGET_FILE:of_shared>
                         ${opengl_lib}
-                        ${ofxKinectLib}             #TODO seperate linking (without Apple)?
+#                        ${ofxKinectLib}             #TODO seperate linking (without Apple)?
 #                        ${ofxAssimpModelLoaderLib}  #TODO seperate linking (without Apple)?
 #                        ${ofxOpenCVLib}             #TODO seperate linking (without Apple)?
                         ${OFX_ADDONS_ACTIVE}
