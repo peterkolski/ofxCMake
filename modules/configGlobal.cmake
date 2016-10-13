68e23a60915cedd43fe09893caf0c41d5cc241b1
Peter Kolski <peter@bildpeter.de>
2016-10-13 14:06:04 +0200

    Global lib properties

--- modules/configGlobal.cmake
+++ modules/configGlobal.cmake
@@ -1,4 +1,4 @@
-
+# --- Was defined OS specifically before
 set( OF_CORE_LIBS
         ${glut_lib}
         ${LIB_POCO_1} ${LIB_POCO_2} ${LIB_POCO_3} ${LIB_POCO_4} ${LIB_POCO_5} ${LIB_POCO_6}
@@ -15,17 +15,36 @@ set( OF_CORE_LIBS
         ${LIB_BOOST_1} ${LIB_BOOST_3}
         )
 
+set( OF_SOURCE_FILES
+        ${OF_SOURCE_FILES}
+        ${ADDON_SOURCE_FILES} )
+include_directories( ${OF_CORE_HEADERS} ${OF_ADDON_HEADERS} )
+set_source_files_properties( ${OF_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "-x objective-c++" )
+add_library( core OBJECT ${OF_SOURCE_FILES} )
 
-set(OF_SOURCE_FILES ${OF_SOURCE_FILES} ${ADDON_SOURCE_FILES})
-include_directories(${OF_CORE_HEADERS} ${OF_ADDON_HEADERS})
-set_source_files_properties(${OF_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "-x objective-c++")
-add_library(core OBJECT ${OF_SOURCE_FILES})
+# ============================================================================
+# -------------------------------- Properties --------------------------------
 
-add_library(static STATIC $<TARGET_OBJECTS:core>)
-set_target_properties(static PROPERTIES OUTPUT_NAME openFrameworks)
-target_link_libraries(static ${OF_CORE_FRAMEWORKS} ${OF_ADDON_FRAMEWORKS} ${OF_CORE_LIBS})
+# -------------------------- Copy OF Libs into CMake/libs --------------------
+#TODO This does not copy the libs
+set_target_properties(  ${openFrameworks}
+        PROPERTIES
+        LIBRARY_OUTPUT_DIRECTORY    ${OF_DIRECTORY_ABSOLUTE}/CMake/libs/
+        #        ARCHIVE_OUTPUT_DIRECTORY    ${OF_DIRECTORY_ABSOLUTE}/CMake/libs/
+        )
+
+add_library(            static STATIC $<TARGET_OBJECTS:core>)
+set_target_properties(  static PROPERTIES OUTPUT_NAME openFrameworks)
+#TODO is OF_CORE_FRAMEWORKS OSX specific?
+target_link_libraries(  static ${OF_CORE_FRAMEWORKS} ${OF_ADDON_FRAMEWORKS} ${OF_CORE_LIBS})
 
 add_library(of_shared SHARED $<TARGET_OBJECTS:core>)
 set_target_properties(of_shared PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/bin/${APP_NAME}.app/Contents/Frameworks)
 set_target_properties(of_shared PROPERTIES OUTPUT_NAME openFrameworks)
 target_link_libraries(of_shared ${OF_CORE_FRAMEWORKS} ${OF_ADDON_FRAMEWORKS} ${OF_CORE_LIBS})
+
+#set_target_properties(  ${static}
+#        PROPERTIES
+#        LIBRARY_OUTPUT_DIRECTORY    ${OF_DIRECTORY_ABSOLUTE}/CMake/libs/
+#        ARCHIVE_OUTPUT_DIRECTORY    ${OF_DIRECTORY_ABSOLUTE}/CMake/libs/
+#        )
--- modules/openFrameworksApple.cmake
+++ modules/openFrameworksApple.cmake
@@ -1,26 +1,26 @@
 # ===================== Libs =====================
-set(glut_lib "${OF_DIRECTORY_ABSOLUTE}/libs/glut/lib/osx/GLUT.framework")
-set(LIB_FMODEX ${OF_DIRECTORY_ABSOLUTE}/libs/fmodex/lib/osx/libfmodex.dylib)
-set(LIB_GLFW ${OF_DIRECTORY_ABSOLUTE}/libs/glfw/lib/osx/glfw3.a)
+set(glut_lib    "${OF_DIRECTORY_ABSOLUTE}/libs/glut/lib/osx/GLUT.framework")
+set(LIB_FMODEX  ${OF_DIRECTORY_ABSOLUTE}/libs/fmodex/lib/osx/libfmodex.dylib)
+set(LIB_GLFW    ${OF_DIRECTORY_ABSOLUTE}/libs/glfw/lib/osx/glfw3.a)
 set(LIB_FREEIMAGE ${OF_DIRECTORY_ABSOLUTE}/libs/FreeImage/lib/osx/freeimage.a)
 set(LIB_FREETYPE ${OF_DIRECTORY_ABSOLUTE}/libs/freetype/lib/osx/freetype.a)
-set(LIB_GLEW ${OF_DIRECTORY_ABSOLUTE}/libs/glew/lib/osx/glew.a)
-set(LIB_POCO_1 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoCrypto.a)
-set(LIB_POCO_2 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoData.a)
-set(LIB_POCO_3 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoJSON.a)
-set(LIB_POCO_4 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoDataSQLite.a)
-set(LIB_POCO_5 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoFoundation.a)
-set(LIB_POCO_6 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoNet.a)
-set(LIB_POCO_7 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoNetSSL.a)
-set(LIB_POCO_8 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoUtil.a)
-set(LIB_POCO_9 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoXML.a)
+set(LIB_GLEW    ${OF_DIRECTORY_ABSOLUTE}/libs/glew/lib/osx/glew.a)
+set(LIB_POCO_1  ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoCrypto.a)
+set(LIB_POCO_2  ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoData.a)
+set(LIB_POCO_3  ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoJSON.a)
+set(LIB_POCO_4  ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoDataSQLite.a)
+set(LIB_POCO_5  ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoFoundation.a)
+set(LIB_POCO_6  ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoNet.a)
+set(LIB_POCO_7  ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoNetSSL.a)
+set(LIB_POCO_8  ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoUtil.a)
+set(LIB_POCO_9  ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoXML.a)
 set(LIB_POCO_10 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoZip.a)
 set(LIB_POCO_11 ${OF_DIRECTORY_ABSOLUTE}/libs/poco/lib/osx/PocoMongoDB.a)
 set(LIB_RTAUDIO ${OF_DIRECTORY_ABSOLUTE}/libs/rtAudio/lib/osx/rtAudio.a)
-set(LIB_TESS ${OF_DIRECTORY_ABSOLUTE}/libs/tess2/lib/osx/tess2.a)
-set(LIB_CAIRO1 ${OF_DIRECTORY_ABSOLUTE}/libs/cairo/lib/osx/cairo-script-interpreter.a)
-set(LIB_CAIRO2 ${OF_DIRECTORY_ABSOLUTE}/libs/cairo/lib/osx/cairo.a)
-set(LIB_CAIRO3 ${OF_DIRECTORY_ABSOLUTE}/libs/cairo/lib/osx/pixman-1.a)
+set(LIB_TESS    ${OF_DIRECTORY_ABSOLUTE}/libs/tess2/lib/osx/tess2.a)
+set(LIB_CAIRO1  ${OF_DIRECTORY_ABSOLUTE}/libs/cairo/lib/osx/cairo-script-interpreter.a)
+set(LIB_CAIRO2  ${OF_DIRECTORY_ABSOLUTE}/libs/cairo/lib/osx/cairo.a)
+set(LIB_CAIRO3  ${OF_DIRECTORY_ABSOLUTE}/libs/cairo/lib/osx/pixman-1.a)
 set(LIB_OPENSSL1 ${OF_DIRECTORY_ABSOLUTE}/libs/openssl/lib/osx/crypto.a)
 set(LIB_OPENSSL2 ${OF_DIRECTORY_ABSOLUTE}/libs/openssl/lib/osx/ssl.a)
 set(LIB_BOOST_1 ${OF_DIRECTORY_ABSOLUTE}/libs/boost/lib/osx/boost.a)
@@ -69,20 +69,6 @@ set(OF_CORE_FRAMEWORKS
         ${appkit_lib})
 # =============================================================
 
-#TODO into configApple
-
-#TODO This does not copy the libs
-set_target_properties(  ${openFrameworks}
-        PROPERTIES
-        LIBRARY_OUTPUT_DIRECTORY    ${OF_DIRECTORY_ABSOLUTE}/CMake/libs/
-#        ARCHIVE_OUTPUT_DIRECTORY    ${OF_DIRECTORY_ABSOLUTE}/CMake/libs/
-        )
-
-#set_target_properties(  ${static}
-#        PROPERTIES
-#        LIBRARY_OUTPUT_DIRECTORY    ${OF_DIRECTORY_ABSOLUTE}/CMake/libs/
-#        ARCHIVE_OUTPUT_DIRECTORY    ${OF_DIRECTORY_ABSOLUTE}/CMake/libs/
-#        )
 
 # TODO Explain the excecutable bindings
 ADD_CUSTOM_COMMAND(TARGET of_shared
