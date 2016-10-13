d4625e3ad964aba184f440669fa5d23ecb1b1bfa
Peter Kolski <peter@bildpeter.de>
2016-10-13 13:12:23 +0200

    Included Linux template

--- modules/openFrameworksGlobal.cmake
+++ modules/openFrameworksGlobal.cmake
@@ -1,27 +1,27 @@
-
 # ----------------------------------------------------------------------------
 # --- Set a absolute path name, is case the project used a relative one
 # ----------------------------------------------------------------------------
 get_filename_component( OF_DIRECTORY_ABSOLUTE ${OF_DIRECTORY_BY_USER} ABSOLUTE)
 
 # ----------------------------------------------------------------------------
+# --------------------------------- ADDONS ----------------------------------
 # --- Include all addOn .cmake files.
 # --- Libs are only linked, if set in the project cMakeLists.txt
 # ----------------------------------------------------------------------------
 include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/addOnsInternal.cmake )
 include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/addOnsExternal.cmake )
 
-
 # ----------------------------------------------------------------------------
-# --- Ask for the operating system
+# ---------------------------- OS SPECIFIC TASKS -----------------------------
 # --- iOS & Android won't be supported
 # ----------------------------------------------------------------------------
 if( APPLE )
     include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/openFrameworksApple.cmake )
     include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/configApple.cmake )
-elseif( UNIX ) # Apple is a Unix, too. So dont ask only UNIX
-
-
+    # Apple is a Unix, too. So dont ask only UNIX
+elseif( UNIX )
+    include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/openFrameworksLinux.cmake )
+    include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/configLinux.cmake  )
 elseif( WIN32)
     include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/openFrameworksWindows.cmake )
     include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/configWindows.cmake  )
