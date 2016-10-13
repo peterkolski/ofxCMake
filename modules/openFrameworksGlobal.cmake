7230b8f922b606524f6a938965ce9604a2424c70
Peter Kolski <peter@bildpeter.de>
2016-10-13 13:04:41 +0200

    Included windows Template

--- modules/openFrameworksGlobal.cmake
+++ modules/openFrameworksGlobal.cmake
@@ -11,10 +11,24 @@ get_filename_component( OF_DIRECTORY_ABSOLUTE ${OF_DIRECTORY_BY_USER} ABSOLUTE)
 include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/addOnsInternal.cmake )
 include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/addOnsExternal.cmake )
 
+
+# ----------------------------------------------------------------------------
+# --- Ask for the operating system
+# --- iOS & Android won't be supported
+# ----------------------------------------------------------------------------
 if( APPLE )
-    include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/configApple.cmake )
     include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/openFrameworksApple.cmake )
+    include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/configApple.cmake )
+elseif( UNIX ) # Apple is a Unix, too. So dont ask only UNIX
+
+
+elseif( WIN32)
+    include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/openFrameworksWindows.cmake )
+    include( ${OF_DIRECTORY_ABSOLUTE}/CMake/modules/configWindows.cmake  )
+else()
+    message( FATAL_ERROR "Operating System not supported" )
 endif()
+
 
 # ===================== App Properties ===================
 set_target_properties(  ${APP_NAME}
