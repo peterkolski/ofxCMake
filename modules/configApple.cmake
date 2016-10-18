945336be58f903ccac4e0eee61aa59a34eb5c096
Peter Kolski <peter@bildpeter.de>
2016-10-18 22:30:31 +0200

    Renundant

--- modules/configApple.cmake
+++ modules/configApple.cmake
@@ -17,12 +17,13 @@ add_compile_options(-Wno-deprecated-declarations)
 # ------------------------------ Compile and Link ----------------------------
 add_executable( ${APP_NAME} MACOSX_BUNDLE ${${APP_NAME}_SOURCE_FILES} )
 
+# TODO ------------ global  - end of main
+
 target_link_libraries(  ${APP_NAME}
                         $<TARGET_FILE:of_shared>
-                        ${opengl_lib}
                         ${OFX_ADDONS_ACTIVE}
                         )
-# ------------ global  - end of main
+
 
 
 
