2fc9b5bc978040dcdd485c9041d17044f0c87d8d
Peter Kolski <peter@bildpeter.de>
2016-10-18 22:40:51 +0200

    OpenGL needs to be included again

--- modules/configApple.cmake
+++ modules/configApple.cmake
@@ -17,15 +17,11 @@ add_compile_options(-Wno-deprecated-declarations)
 # ------------------------------ Compile and Link ----------------------------
 add_executable( ${APP_NAME} MACOSX_BUNDLE ${${APP_NAME}_SOURCE_FILES} )
 
-# TODO ------------ global  - end of main
-
 target_link_libraries(  ${APP_NAME}
                         $<TARGET_FILE:of_shared>
+                        ${opengl_lib}               # TODO Why is this needed here?
                         ${OFX_ADDONS_ACTIVE}
                         )
-
-
-
 
 # ============================================================================
 ADD_CUSTOM_COMMAND( TARGET ${APP_NAME}
