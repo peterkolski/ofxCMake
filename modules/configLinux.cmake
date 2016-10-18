1eb3e9187596f99e90757ec6a4c20dd71bee95d0
Peter Kolski <peter@bildpeter.de>
2016-10-18 21:45:01 +0200

    CleanUp

--- modules/configLinux.cmake
+++ modules/configLinux.cmake
@@ -1,16 +1,20 @@
 
-
+# ============================================================================
+# ------------------------------ Compiler Flags ------------------------------
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -Wall")
 #set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
 
 
+# ============================================================================
+# ------------------------------ Compile and Link ----------------------------
+add_executable( ${APP_NAME} ${SOURCE_FILES} )
 
+target_link_libraries( ${APP_NAME}
+                        $<TARGET_FILE:of_shared>
+                        ${OF_CORE_LIBS}
+                        ${OFX_ADDONS_ACTIVE}
+                    )
 
-add_executable(${APP_NAME} ${SOURCE_FILES})
+# global - end of main
 
-add_dependencies(${APP_NAME} of_shared)
-target_link_libraries(${APP_NAME} $<TARGET_FILE:of_shared>)
-target_link_libraries(${APP_NAME} ${OF_CORE_LIBS})
-
-
-set_target_properties(${APP_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/bin)
+add_dependencies( ${APP_NAME} of_shared )
