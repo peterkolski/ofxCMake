1169c69d19b6a153093817c3299b88bc76a63101
Peter Kolski <peter@bildpeter.de>
2016-10-24 13:44:33 +0200

    Already existing external add ons can be used

--- modules/addOnsExternal.cmake
+++ modules/addOnsExternal.cmake
@@ -1,5 +1,16 @@
 
-#include( ${OF_CMAKE_ADDONS}/external/ofxTwistedRibbon.cmake )
-#include( ${OF_CMAKE_ADDONS}/external/ofxOneEuroFilter.cmake )
-#include( ${OF_CMAKE_ADDONS}/external/ofxFlowTools.cmake )
-#include( ${OF_CMAKE_ADDONS}/external/ofxMidi.cmake )
+if( EXISTS ${OF_DIRECTORY_ABSOLUTE}/addons/ofxTwistedRibbon/ )
+    include( ${OF_CMAKE_ADDONS}/external/ofxTwistedRibbon.cmake )
+endif()
+
+if( EXISTS ${OF_DIRECTORY_ABSOLUTE}/addons/ofxOneEuroFilter/ )
+include( ${OF_CMAKE_ADDONS}/external/ofxOneEuroFilter.cmake )
+endif()
+
+if( EXISTS ${OF_DIRECTORY_ABSOLUTE}/addons/ofxFlowTools/ )
+    include( ${OF_CMAKE_ADDONS}/external/ofxFlowTools.cmake )
+endif()
+
+if( EXISTS ${OF_DIRECTORY_ABSOLUTE}/addons/ofxMidi/ )
+    include( ${OF_CMAKE_ADDONS}/external/ofxMidi.cmake )
+endif()
