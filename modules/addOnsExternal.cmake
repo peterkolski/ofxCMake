5e98c2ada9abf4961b912b3f13283020ec9691d4
Peter Kolski <peter@bildpeter.de>
2016-10-25 11:36:59 +0200

    Algorithm to activate external addOns

--- modules/addOnsExternal.cmake
+++ modules/addOnsExternal.cmake
@@ -1,10 +1,13 @@
 
-if( EXISTS ${OF_DIRECTORY_ABSOLUTE}/addons/ofxTwistedRibbon/ )
-    include( ${OF_CMAKE_ADDONS}/external/ofxTwistedRibbon.cmake )
+set( NAME_ADDON ofxTwistedRibbon )
+if( EXISTS ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/
+    AND ${NAME_ADDON} IN_LIST OFX_ADDONS_ACTIVE)
+    include( ${OF_CMAKE_ADDONS}/external/${NAME_ADDON}.cmake )
+    message( WARNING "${NAME_ADDON} activated" )
 endif()
 
 if( EXISTS ${OF_DIRECTORY_ABSOLUTE}/addons/ofxOneEuroFilter/ )
-include( ${OF_CMAKE_ADDONS}/external/ofxOneEuroFilter.cmake )
+    include( ${OF_CMAKE_ADDONS}/external/ofxOneEuroFilter.cmake )
 endif()
 
 if( EXISTS ${OF_DIRECTORY_ABSOLUTE}/addons/ofxFlowTools/ )
