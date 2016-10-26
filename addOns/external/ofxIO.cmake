715524deaf4f70117a1cb84cc2206f50e988d6af
Peter Kolski <peter@bildpeter.de>
2016-10-26 22:47:49 +0200

    Added ofxIO template

---
+++ addOns/external/ofxIO.cmake
@@ -0,0 +1,42 @@
+# -----------------------------------------------------------------
+# --- Set the name of your AddOn
+# -----------------------------------------------------------------
+
+set( NAME_ADDON     ofxIO )
+
+#==================================================================
+#==================================================================
+# -----------------------------------------------------------------
+# ---------------------------- PATHS ------------------------------
+# -----------------------------------------------------------------
+set( PATH_SOURCE    ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/src )
+set( PATH_LIBS      ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/libs )
+
+# --- Setting abolute path to prevent errors
+get_filename_component( PATH_SOURCE_ABSOLUTE ${PATH_SOURCE} ABSOLUTE)
+get_filename_component( PATH_LIBS_ABSOLUTE ${PATH_LIBS} ABSOLUTE)
+
+# -----------------------------------------------------------------
+# ---------------------------- SOURCE -----------------------------
+# -----------------------------------------------------------------
+
+file( GLOB_RECURSE   OFX_ADDON_CPP   "${PATH_SOURCE_ABSOLUTE}/*.cpp" )
+add_library(  ${NAME_ADDON}   STATIC   ${OFX_ADDON_CPP} )
+#message( WARNING "CPP ${NAME_ADDON}: ${OFX_ADDON_CPP}" )
+
+# -----------------------------------------------------------------
+# ---------------------------- HEADERS ----------------------------
+# -----------------------------------------------------------------
+
+find_header_directories( HEADERS_SOURCE ${PATH_SOURCE_ABSOLUTE} )
+include_directories( ${HEADERS_SOURCE} )
+
+find_header_directories( HEADERS_LIBS ${PATH_LIBS_ABSOLUTE} )
+include_directories( ${HEADERS_LIBS} )
+
+message( WARNING "Header paths ${NAME_ADDON}: ${HEADERS_LIBS}" )
+
+
+# -----------------------------------------------------------------
+# ------------------------------ LIBS  ----------------------------
+# -----------------------------------------------------------------
