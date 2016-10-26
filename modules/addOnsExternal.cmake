5e2135025e5d4c340f5f0b2f246c9dc78b29c885
Peter Kolski <peter@bildpeter.de>
2016-10-26 14:09:08 +0200

    Included ofxPiMapper empty .cmake

--- modules/addOnsExternal.cmake
+++ modules/addOnsExternal.cmake
@@ -35,3 +35,11 @@ else()
     message( WARNING "${NAME_ADDON} folder not found" )
 endif()
 
+set( NAME_ADDON ofxPiMapper )
+if( EXISTS ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/
+        AND ${NAME_ADDON} IN_LIST OFX_ADDONS_ACTIVE)
+    include( ${OF_CMAKE_ADDONS}/external/${NAME_ADDON}.cmake )
+    message( STATUS "${NAME_ADDON} activated" )
+else()
+    message( WARNING "${NAME_ADDON} folder not found" )
+endif()
