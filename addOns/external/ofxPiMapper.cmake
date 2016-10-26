db616d3a76a9719e25d50f30cb09b987a06027b7
Peter Kolski <peter@bildpeter.de>
2016-10-26 22:39:26 +0200

    Added Libs & Cleanup

--- addOns/external/ofxPiMapper.cmake
+++ addOns/external/ofxPiMapper.cmake
@@ -3,29 +3,43 @@
 # -----------------------------------------------------------------
 
 set( NAME_ADDON     ofxPiMapper )
+
+#==================================================================
+#==================================================================
+# -----------------------------------------------------------------
+# ---------------------------- PATHS ------------------------------
+# -----------------------------------------------------------------
 set( PATH_SOURCE    ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/src )
-#set( PATH_LIBS      ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/libs )
-#message( WARNING "Path ${NAME_ADDON}: ${PATH_SOURCE}")
+set( PATH_LIBS      ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/libs )
+message( WARNING "Path ${NAME_ADDON}: ${PATH_LIBS}")
+
+# --- Setting abolute path to prevent errors
+get_filename_component( PATH_SOURCE_ABSOLUTE ${PATH_SOURCE} ABSOLUTE)
+get_filename_component( PATH_LIBS_ABSOLUTE ${PATH_LIBS} ABSOLUTE)
+
+message( WARNING "Path absolute ${NAME_ADDON}: ${PATH_LIBS_ABSOLUTE}")
+
+# -----------------------------------------------------------------
+# ---------------------------- SOURCE -----------------------------
 # -----------------------------------------------------------------
 
-get_filename_component( PATH_SOURCE_ABSOLUTE ${PATH_SOURCE} ABSOLUTE)
-#message( WARNING "Path absolute ${NAME_ADDON}: ${PATH_SOURCE}")
-
 file( GLOB_RECURSE   OFX_ADDON_CPP   "${PATH_SOURCE_ABSOLUTE}/*.cpp" )
+add_library(  ${NAME_ADDON}   STATIC   ${OFX_ADDON_CPP} )
 #message( WARNING "CPP ${NAME_ADDON}: ${OFX_ADDON_CPP}" )
 
 # -----------------------------------------------------------------
-# --- Set ALL directories of the addOn, which contain .h files
+# ---------------------------- HEADERS ----------------------------
 # -----------------------------------------------------------------
 
-HEADER_DIRECTORIES( headerList ${PATH_SOURCE_ABSOLUTE} )
+HEADER_DIRECTORIES( HEADERS_SOURCE ${PATH_SOURCE_ABSOLUTE} )
+include_directories( ${HEADERS_SOURCE} )
 
-include_directories( ${headerList} )
-message( WARNING "Header paths ${NAME_ADDON}: ${headerList}" )
+HEADER_DIRECTORIES( HEADERS_LIBS ${PATH_LIBS_ABSOLUTE} )
+include_directories( ${HEADERS_LIBS} )
+
+message( WARNING "Header paths ${NAME_ADDON}: ${HEADERS_LIBS}" )
+
 
 # -----------------------------------------------------------------
-# --- Set the keyword, so you can simply include the addOn
+# ------------------------------ LIBS  ----------------------------
 # -----------------------------------------------------------------
-
-
-add_library(  ${NAME_ADDON}   STATIC   ${OFX_ADDON_CPP} )
