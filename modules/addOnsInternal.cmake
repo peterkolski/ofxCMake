93f6a066842090b99b9cb7159f71cc1c477dc1e9
Peter Kolski <peter@bildpeter.de>
2016-11-01 15:22:19 +0100

    Macro also for internal addOns

--- modules/addOnsInternal.cmake
+++ modules/addOnsInternal.cmake
@@ -1,48 +1,13 @@
-# TODO Comments
+OF_include_internal_addOn( ofxOsc )
+OF_include_internal_addOn( ofxXmlSettings )
+OF_include_internal_addOn( ofxGui )
+OF_include_internal_addOn( ofxKinect )
+OF_include_internal_addOn( ofxNetwork )
+OF_include_internal_addOn( ofxSvg )
+OF_include_internal_addOn( ofxVectorGraphics )
+OF_include_internal_addOn( ofxOpenCv )
+OF_include_internal_addOn( ofxThreadedImageLoader )
+OF_include_internal_addOn( ofxAssimpModelLoader )
+OF_include_internal_addOn( ofxEmscripten )
 
-if ("ofxOsc" IN_LIST OFX_ADDONS_ACTIVE)
-    include( ${OF_CMAKE_ADDONS}/internal/ofxOsc.cmake )
-    message( STATUS "ofxOSC included" )
-endif()
-if ( "ofxXmlSettings" IN_LIST OFX_ADDONS_ACTIVE )
-    include( ${OF_CMAKE_ADDONS}/internal/ofxXmlSettings.cmake )
-endif()
-if ( "ofxGui" IN_LIST OFX_ADDONS_ACTIVE )
-    include( ${OF_CMAKE_ADDONS}/internal/ofxGui.cmake )
-endif()
-if ( "ofxKinect" IN_LIST OFX_ADDONS_ACTIVE )
-    include( ${OF_CMAKE_ADDONS}/internal/ofxKinect.cmake )
-endif()
-if ( "ofxNetwork" IN_LIST OFX_ADDONS_ACTIVE )
-    include( ${OF_CMAKE_ADDONS}/internal/ofxNetwork.cmake )
-endif()
-if ( "ofxSvg" IN_LIST OFX_ADDONS_ACTIVE )
-    include( ${OF_CMAKE_ADDONS}/internal/ofxSvg.cmake )
-endif()
 
-if ( "ofxVectorGraphics" IN_LIST OFX_ADDONS_ACTIVE )
-    include( ${OF_CMAKE_ADDONS}/internal/ofxVectorGraphics.cmake )
-endif()
-
-if ( "ofxOpenCv" IN_LIST OFX_ADDONS_ACTIVE )
-    include( ${OF_CMAKE_ADDONS}/internal/ofxOpenCv.cmake )
-endif()
-
-if ( "ofxThreadedImageLoader" IN_LIST OFX_ADDONS_ACTIVE )
-    include( ${OF_CMAKE_ADDONS}/internal/ofxThreadedImageLoader.cmake )
-endif()
-
-if ( "ofxAssimpModelLoader" IN_LIST OFX_ADDONS_ACTIVE )
-    include( ${OF_CMAKE_ADDONS}/internal/ofxAssimpModelLoader.cmake )
-endif()
-
-#if ( "" IN_LIST OFX_ADDONS_ACTIVE )
-#include( ${OF_CMAKE_ADDONS}internal/ofxEmscripten.cmake )
-
-# TODO automatic search for addOns .cmake files
-#file( GLOB_RECURSE ALL_ADDON_FILES  "${OF_CMAKE_ADDONS}*.cmake" )
-#include( ALL_ADDON_FILES )
-
-# including all cpp files recursively
-#file( GLOB_RECURSE OFX_OSC_CPP  "${OF_CMAKE_ADDONS}ofxOsc/*.cpp" )
-#message( "Cpp-Tree: ${OFX_OSC_CPP}" )
--- modules/addOnsMacros.cmake
+++ modules/addOnsMacros.cmake
@@ -1,4 +1,16 @@
 #==================================================================
+# ---- activate internal addOn
+macro( OF_include_internal_addOn NAME_ADDON )
+    if( ${NAME_ADDON} IN_LIST OFX_ADDONS_ACTIVE )
+        if( EXISTS ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/)
+            include( ${OF_CMAKE_ADDONS}/internal/${NAME_ADDON}.cmake )
+            message( STATUS "${NAME_ADDON} activated" )
+        else()
+            message( WARNING "${NAME_ADDON} folder not found" )
+        endif()
+    endif()
+endmacro( OF_include_internal_addOn )
+
 # ---- activate external addOn
 macro( include_external_addOn NAME_ADDON )
     if( ${NAME_ADDON} IN_LIST OFX_ADDONS_ACTIVE )
@@ -36,3 +48,13 @@ macro( find_source_files return_list PATH )
     file( GLOB_RECURSE   FILES_C     "${PATH}/*.c" )
     set( ${return_list} ${FILES_CPP} ${FILES_CC} ${FILES_C} )
 endmacro()
+
+#==================================================================
+
+# TODO automatic search for addOns .cmake files
+#file( GLOB_RECURSE ALL_ADDON_FILES  "${OF_CMAKE_ADDONS}*.cmake" )
+#include( ALL_ADDON_FILES )
+
+# including all cpp files recursively
+#file( GLOB_RECURSE OFX_OSC_CPP  "${OF_CMAKE_ADDONS}ofxOsc/*.cpp" )
+#message( "Cpp-Tree: ${OFX_OSC_CPP}" )
