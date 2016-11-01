6d8991166804752fdf28ecad040c2e1065d23e41
Peter Kolski <peter@bildpeter.de>
2016-11-01 18:02:13 +0100

    Removed external addOns file

--- modules/addOnsExternal.cmake
+++
@@ -1,8 +0,0 @@
-
-OF_include_external_addOn( ofxTwistedRibbon )
-OF_include_external_addOn( ofxOneEuroFilter )
-OF_include_external_addOn( ofxFlowTools )
-OF_include_external_addOn( ofxMidi )
-OF_include_external_addOn( ofxPiMapper )
-OF_include_external_addOn( ofxIO )
-OF_include_external_addOn( ofxQuadWarp )
