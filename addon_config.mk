21999cbecacddcb888a28a9669c9ac4fb3ba6051
Peter Kolski <peter@bildpeter.de>
2017-01-10 15:02:14 +0100

    MetaData config

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxCMake
+	ADDON_DESCRIPTION = Compilation of OpenFrameworks via CMake. Use of CLion possible
+	ADDON_AUTHOR = Peter Kolski
+	ADDON_TAGS =  "Utilities" "cMake" "CLion"
+	ADDON_URL = https://github.com/BildPeter/ofxCMake
