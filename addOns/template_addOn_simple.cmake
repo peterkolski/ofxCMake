3911e3cff3e28e30039a91029a15b7f5351ea86e
Peter Kolski <peter@bildpeter.de>
2017-01-10 20:17:41 +0100

    Shortened the addon template

--- addOns/template_addOn_simple.cmake
+++ addOns/template_addOn_simple.cmake
@@ -16,27 +16,23 @@ set( NAME_ADDON     ofxNAME )       # <--- Set the name here
 # -----------------------------------------------------------------
 # ---------------------------- PATHS ------------------------------
 # -----------------------------------------------------------------
-set( PATH_SOURCE    ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/src )
+set( PATH_SRC    ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/src )
 set( PATH_LIBS      ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/libs )
-
-# --- Setting abolute path to prevent errors
-get_filename_component( PATH_SOURCE_ABSOLUTE ${PATH_SOURCE} ABSOLUTE)
-get_filename_component( PATH_LIBS_ABSOLUTE ${PATH_LIBS} ABSOLUTE)
 
 # -----------------------------------------------------------------
 # ---------------------------- SOURCE -----------------------------
 # -----------------------------------------------------------------
 
-file( GLOB_RECURSE   OFX_ADDON_CPP          "${PATH_SOURCE_ABSOLUTE}/*.cpp" )
-file( GLOB_RECURSE   OFX_ADDON_LIBS_CPP     "${PATH_LIBS_ABSOLUTE}/*.cpp" )
+file( GLOB_RECURSE   OFX_ADDON_CPP          "${PATH_SRC}/*.cpp" )
+file( GLOB_RECURSE   OFX_ADDON_LIBS_CPP     "${PATH_LIBS}/*.cpp" )
 add_library(  ${NAME_ADDON}   STATIC   ${OFX_ADDON_CPP} ${OFX_ADDON_LIBS_CPP} )
 
 # -----------------------------------------------------------------
 # ---------------------------- HEADERS ----------------------------
 # -----------------------------------------------------------------
 
-OF_find_header_directories( HEADERS_SOURCE ${PATH_SOURCE_ABSOLUTE} )
-OF_find_header_directories( HEADERS_LIBS ${PATH_LIBS_ABSOLUTE} )
+OF_find_header_directories( HEADERS_SOURCE ${PATH_SRC} )
+OF_find_header_directories( HEADERS_LIBS ${PATH_LIBS} )
 
 include_directories( ${HEADERS_SOURCE} )
 include_directories( ${HEADERS_LIBS} )
