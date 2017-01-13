02b2f7ca25cd13f273616dc7738df65e532f7e73
Peter Kolski <peter@bildpeter.de>
2017-01-13 13:14:48 +0100

    ofxDatGui addon

--- modules/addOns.cmake
+++ modules/addOns.cmake
@@ -24,4 +24,6 @@ OF_include_external_addOn( ofxQuadWarp )
 OF_include_external_addOn( ofxGrt )
 OF_include_external_addOn( ofxEliza )
 OF_include_external_addOn( ofxSpeech )
+OF_include_external_addOn( ofxDatGui )
+
 
