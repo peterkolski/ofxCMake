04f2cd19862b5bd02a97bff4357111bfbfe93f7b
Peter Kolski <peter@bildpeter.de>
2017-01-25 18:23:54 +0100

    Two new externals

---
+++ addOns/external/ofxSoundProcessor.cmake
@@ -0,0 +1,38 @@
+# =================================================================
+#
+# Template for addOns
+#
+# Easiest way: replace all the NAME into the name of your addOn
+# It has to have the same name, as the directory it is in
+# [e.g. ofxOneEuroFilter is in of/addons/ofxOneEuroFilter/]
+#
+# =================================================================
+
+set( NAME_ADDON     ofxSoundProcessor )       # <--- Set the name here
+
+#==================================================================
+
+
+# -----------------------------------------------------------------
+# ---------------------------- PATHS ------------------------------
+# -----------------------------------------------------------------
+set( PATH_SRC    ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/src )
+set( PATH_LIBS      ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/libs )
+
+# -----------------------------------------------------------------
+# ---------------------------- SOURCE -----------------------------
+# -----------------------------------------------------------------
+
+file( GLOB_RECURSE   OFX_ADDON_CPP          "${PATH_SRC}/*.cpp" )
+file( GLOB_RECURSE   OFX_ADDON_LIBS_CPP     "${PATH_LIBS}/*.cpp" )
+add_library(  ${NAME_ADDON}   STATIC   ${OFX_ADDON_CPP} ${OFX_ADDON_LIBS_CPP} )
+
+# -----------------------------------------------------------------
+# ---------------------------- HEADERS ----------------------------
+# -----------------------------------------------------------------
+
+OF_find_header_directories( HEADERS_SOURCE ${PATH_SRC} )
+OF_find_header_directories( HEADERS_LIBS ${PATH_LIBS} )
+
+include_directories( ${HEADERS_SOURCE} )
+include_directories( ${HEADERS_LIBS} )
--- modules/addOns.cmake
+++ modules/addOns.cmake
@@ -24,6 +24,7 @@ OF_include_external_addOn( ofxQuadWarp )
 OF_include_external_addOn( ofxGrt )
 OF_include_external_addOn( ofxEliza )
 OF_include_external_addOn( ofxSpeech )
-OF_include_external_addOn( ofxDatGui )
+OF_include_external_addOn( ofxUI )
+OF_include_external_addOn( ofxSoundProcessor )
 
 
