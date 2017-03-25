b7d72969b9d4c6e69fd2dc9cf3828f28aa67b73f
Joshua Levine <joshua.a.levine@gmail.com>
2017-03-25 09:48:01 -0700

    Adding support for ofxDatGui

--- modules/addOns.cmake
+++ modules/addOns.cmake
@@ -26,6 +26,6 @@ OF_include_external_addOn( ofxEliza )
 OF_include_external_addOn( ofxSpeech )
 OF_include_external_addOn( ofxUI )
 OF_include_external_addOn( ofxSoundProcessor )
-OF_include_external_addOn( ofxSyphon )
+OF_include_external_addOn( ofxDatGui )
 
 
